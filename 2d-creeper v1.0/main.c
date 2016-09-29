7ccd888703a9ffc099e9c072e7f09a2dc7f39f08
kim taeyun <x86poem@gmail.com>
2016-09-29 13:39:11 +0900

    sight

--- 2d-creeper v1.0/main.c
+++ 2d-creeper v1.0/main.c
@@ -15,28 +15,84 @@ typedef struct Actor
 } Actor;
 Actor actors[100];
 
+void set_cursor(int visibility)
+{
+	HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_CURSOR_INFO info;
+	info.dwSize = 100;
+	if(visibility == 0)
+		info.bVisible = 0;
+	else
+		info.bVisible = 1;
+	SetConsoleCursorInfo(h,&info);
+}
 int boundary_ok(int x, int y)
 {
 	if(x<0||y<0 || x>=80||y>=24)
 		return 0;
 	return 1;
 }
+
+int in_hero_sight(Actor* hero, Actor * a)
+{
+	int from_x = hero->x - 5;
+	int to_x = hero->x +5;
+	int from_y = hero->y -5;
+	int to_y = hero->y +5;
+	
+	if(a->x >= from_x && a->x <=to_x 
+		&& a->y >=from_y && a->y <= to_y)
+		return 1;
+		
+	return 0;
+	
+}
+
+Actor* get_actor_at(Actor* actors,int x, int y)
+{
+	for(int i = 0;i<100;i++)
+	{
+		Actor* a = &(actors[i]);
+		if(a->x == x && a->y == y)
+			return a;
+	}
+	return 0;
+}
 void draw(Actor* actors)
 {
     CHAR_INFO buffer[24][80];
-    memset(buffer,0,sizeof(CHAR_INFO)*24*80);
-    int i;
-    for(i =0;i<100;i++)
-    {
-        if(actors[i].exist == 0)
-            continue;
-        int x = actors[i].x;
-        int y =actors[i].y;
-		if(boundary_ok(x,y) ==0 )
-			continue;
-        buffer[y][x].Char.AsciiChar = actors[i].shape;
-        buffer[y][x].Attributes = actors[i].color;
-    }
+    for(int cy = 0; cy<24;cy++)
+	{
+		for(int cx = 0; cx<80;cx++)
+		{
+			buffer[cy][cx].Char.AsciiChar = ' ';
+			buffer[cy][cx].Attributes = (15-8) <<4;
+		}
+	}
+	
+	Actor* hero = &(actors[0]);
+	for(int cy = 0;cy<10;cy++)
+	{
+		for(int cx = 0;cx<20;cx++)
+		{
+			//sight_x와 y는 hero를 가장 왼쪽 위로 해서 10x20이 아니라
+			//hero의 -10왼쪽 -5위 쪽을 사각형의 왼족 위로 해서 10x20
+			int sight_x = hero->x +cx -10;
+			int sight_y = hero->y +cy -5;
+			Actor* a = get_actor_at(actors,sight_x,sight_y);
+			if(a == 0)
+			{
+				buffer[cy][cx].Char.AsciiChar = ' ';
+				buffer[cy][cx].Attributes = 0;
+			}
+			else
+			{				
+				buffer[cy][cx].Char.AsciiChar = a->shape;
+				buffer[cy][cx].Attributes = a->color;
+			}
+		}
+	}
+	
     HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
     COORD sz = {80,24};
     COORD xy = {0,0};
@@ -126,7 +182,9 @@ int main()
     int creeper;
     srand(time(0));
     memset(actors,0,sizeof(Actor)*100);
-
+	
+	set_cursor(0);
+	
     actors[0] = new_hero(10,10);
     printf("크리퍼를 몇 마리 만드시겠습니까? : ");
     scanf("%d", &creeper);
@@ -139,7 +197,7 @@ int main()
     while(1)
     {
         keyboard(hero);
-		tick(actors);
+		//tick(actors);
         draw(actors);
         Sleep(100);
     }
